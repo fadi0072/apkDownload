9d194346bb137adaf7d9eb46d633d7f43f14c577
fadi0072 <76230134+fadi0072@users.noreply.github.com>
2022-08-31 01:31:21 +0500

    createdApp

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Image, StyleSheet, Text, View, ScrollView, TouchableOpacity, ActivityIndicator, Linking } from 'react-native';
+import { instance } from './Api/axios';
+import RNApkInstallerN from 'react-native-apk-installer-n';
+import * as FileSystem from 'expo-file-system';
+
 
 export default function App() {
+  const [data, setData] = useState("")
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = async () => {
+
+    try {
+      const response = await instance.get("apiG0")
+      console.log('response is', response.data)
+      setData(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const renderCard = (item, i) => {
+    return (
+      <View style={styles.cardVeiw}>
+        <TouchableOpacity style={styles.cardVeiw} onPress={() => { Linking.openURL(`http://play.google.com/store/apps/details?id=${item.apkpackagename}`) }}>
+          <Image source={{ uri: item.apkicon }} style={{ width: 200, height: 200 }} />
+          <View style={{ alignItems: 'center', width: '100%' }}>
+            <Text style={styles.appName}>{item.apkname}</Text>
+            <Text style={styles.appVersion}>{item.apkversionname}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {data == "" || data == undefined ?
+          <ActivityIndicator
+            size="small"
+            color="white"
+          /> :
+          data.map(renderCard)
+        }
+        {/* {renderCard()} */}
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#202124',
+
   },
+  cardVeiw: {
+    marginHorizontal: '10%',
+    marginVertical: '15%',
+    backgroundColor: 'red',
+    alignItems: 'center'
+  },
+  appName: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: '800'
+  },
+  appVersion: {
+    fontSize: 15,
+    color: 'white',
+    fontWeight: '300'
+  }
 });
